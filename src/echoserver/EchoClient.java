b03d5454d057e2fa537e0c4b6e8316309e38dfa8
chong050 <chong050@morris.umn.edu>
2018-10-16 11:29:06 -0500

    Create EchoClient.java. 2/5 tests pass. Issue stems from try catch loop.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,46 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
 public class EchoClient {
+  public static final int portNumber = 6013;
+
+  public static void main(String[] args) throws IOException {
+    String server;
+
+    if (args.length == 0) {
+      server = "127.0.0.1";
+    } else {
+      server = args[0];
+    }
+
+    try {
+      Socket socket = new Socket(server, portNumber);
+
+      DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
+      Scanner scanner = new Scanner(System.in);
+      byte k;
+      DataInputStream inStream = new DataInputStream(socket.getInputStream());
+      byte b;
+
+      while(scanner.hasNextByte()){
+        k = scanner.nextByte();
+        outStream.writeByte(k);
+        b = inStream.readByte();
+        System.out.print(b);
+      }
+
+      socket.close();
+
+    } catch (ConnectException ce) {
+      System.out.println("We were unable to connect to " + server);
+      System.out.println("You should make sure the server is running.");
+    } catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
+    }
+
+  }
 }
