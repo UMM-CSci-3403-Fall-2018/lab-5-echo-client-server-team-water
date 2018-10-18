c3eb88dd42a821d887d459825fe4b3f77ea711c9
chong050 <chong050@morris.umn.edu>
2018-10-18 11:22:55 -0500

    Write EchoServer.java. use DateServer example as ref. All tests pass.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,33 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+  public static final int portNumber = 6013;
+
+  public static void main(String[] args) {
+    try {
+      // Start listening on specified port
+      ServerSocket socket = new ServerSocket(portNumber);
+
+      while (true) {
+        Socket client = socket.accept();
+        System.out.println("Client connected");
+
+        // c for client
+        int c;
+
+        while ((c = client.getInputStream().read()) != -1) {
+          client.getOutputStream().write((byte) c);
+        }
+
+        client.close();
+        System.out.println("Client disconnected");
+      }
+    } catch (IOException ioe) {
+      System.out.println(":(");
+      System.err.println(ioe);
+    }
+  }
 }
